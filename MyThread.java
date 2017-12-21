e41cc92194789ae15d8cacac0274b0b688293217
Mario Raciti <23482292+zMrDevJ@users.noreply.github.com>
2017-12-21 17:06:50 +0100

    Create MyThread.java

---
+++ MyThread.java
@@ -0,0 +1,78 @@
+/*********************************
+ *
+ *  Title:  Simple Threads Implementation
+ *  Author: zMrDevJ
+ *  Date: 12/21/2017
+ *
+ ********************************/
+
+/* Main class */
+public class MyThread{
+
+	/* Main */
+	public static void main(String[] args) {
+		/* Shared data */
+		Shared shared = new Shared();
+
+		/* Create threads */
+		Thread t1 = new Thread(new T1(shared), "t1");
+		Thread t2 = new Thread(new T2(shared), "t2");
+
+		/* Start threads */
+		t1.start();
+		t2.start();
+
+	}
+}
+
+/* Shared data and mutex class */
+class Shared{
+	int shared = 100;
+
+	/* Synchronized method */
+	public synchronized void method(){
+		/* Get thread name */
+		String thread = Thread.currentThread().getName();
+		System.out.println("Thread: " + thread);
+	}
+}
+
+/* Thread 1 class */
+class T1 implements Runnable{
+	/* Shared data */
+	Shared shared;
+
+	/* Constructor */
+	public T1(Shared shared) { this.shared = shared; }
+
+	/* Run method */
+	public void run(){
+		try{
+			Thread.sleep(100);
+		}catch(InterruptedException ie){
+			ie.printStackTrace();
+		}
+
+		shared.method();
+	}
+}
+
+/* Thread 2 class */
+class T2 implements Runnable{
+	/* Shared data */
+	Shared shared;
+
+	/* Constructor */
+	public T2(Shared shared) { this.shared = shared; }
+
+	/* Run method */
+	public void run(){
+		try{
+			Thread.sleep(100);
+		}catch(InterruptedException ie){
+			ie.printStackTrace();
+		}
+
+		shared.method();
+	}
+}
